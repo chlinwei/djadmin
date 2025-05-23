ce126c4dbb4333719c6265d63c94228fe30092a1
chlinwei <862350707@qq.com>
2025-05-23 17:36:00 +0800

    用户管理2

--- fronted/src/main.js
+++ fronted/src/main.js
@@ -12,11 +12,22 @@ import '@/assets/styles/border.css'
 import '@/assets/styles/reset.css'
 import {addDynamicRoutes} from '@/router/index.js';
 
+// fontawesome 组件注册,这里全局注册不起作用啊
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEdit,faTrash,faUserSecret,faTree } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash)
+library.add(faEdit)
+library.add(faTree)
+
 
 //icon
 import {setup as setupIcon} from './components/SvgIcon/index.js'
 
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+// app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.use(store)
 //解决刷新页面后，出现空白的情况，这里是直接加载动态路由
 addDynamicRoutes()
--- fronted/src/views/sys/user/index.vue
+++ fronted/src/views/sys/user/index.vue
@@ -1,40 +1,4 @@
-<template>
-  <a-table :columns="columns" :data-source="users">
-    <template #headerCell="{ column }">
-      <template v-if="column.key === 'name'">
-        <span>
-          Name
-        </span>
-      </template>
-    </template>
 
-    <template #bodyCell="{ column, record }">
-      <template v-if="column.key === 'name'">
-        <a>
-          {{ record.name }}
-        </a>
-      </template>
-      <template v-else-if="column.key === 'roles'">
-        <span>
-          <a-tag
-            v-for="role in record.roles"
-            :key="role.id"
-          >
-            {{ role.name }}
-          </a-tag>
-        </span>
-      </template>
-      <template v-else-if="column.key === 'action'">
-        <span>
-          <a>分配角色</a>
-          <a>重置密码</a>
-          <a>修改用户</a>
-          <a>删除用户</a>
-        </span>
-      </template>
-    </template>
-  </a-table>
-</template>
   
   <script setup>
 
@@ -44,10 +8,11 @@
 })
   import { ref } from 'vue'
   import { getUserList } from '@/api/user/index.js';
-  
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  import { faEdit } from '@fortawesome/free-solid-svg-icons'
+  import { faTrash } from '@fortawesome/free-solid-svg-icons'
   const users = ref([])
   const columns = [
-    { title: 'id', dataIndex: 'id',key: 'id' },
     { title: '用户名', dataIndex: 'username',key: 'username' },
     { title: '角色', dataIndex: 'roles',key: 'roles' },
     { title: '邮箱', dataIndex: 'email',key: 'email' },
@@ -67,5 +32,81 @@ getUserList().then((result)=>{
   const editUser = (user) => {
     // 编辑逻辑
   }
+
+  const changeStatus = (e) => {
+    
+  }
   </script>
+<template>
+  <a-row class="search" :gutter="16">
+    <a-col :span="7">
+    <a-input-search
+      v-model:value="value"
+      placeholder="全局搜索"
+      enter-button
+      @search="onSearch"
+    />
+
+    </a-col>
+  </a-row>
+  <a-table :columns="columns" :data-source="users">
+    <template #headerCell="{ column }">
+      <template v-if="column.key === 'name'">
+        <span>
+          Name
+        </span>
+      </template>
+    </template>
+
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'name'">
+        <a>
+          {{ record.name }}
+        </a>
+      </template>
+      <template v-else-if="column.key === 'roles'">
+        <span>
+          <a-tag color="orange"
+            v-for="role in record.roles"
+            :key="role.id"
+          >
+            {{ role.name }}
+          </a-tag>
+        </span>
+      </template>
+            <template v-else-if="column.key === 'status'">
+        <span>
+          <a-switch :checked="record.status === 1"  @change="changeStatus" />
+          
+        </span>
+      </template>
+      <template v-else-if="column.key === 'action'">
+        <span>
+          <a-button type="primary" id="assignRole">分配角色</a-button>
+          <div id="resetPwd" style="display: inline;">
+              <a-button >重置密码</a-button>   
+          </div>
+          <FontAwesomeIcon :icon="faEdit" />
+          <FontAwesomeIcon :icon="faTrash" />
+      
+        </span>
+      </template>
+    </template>
+  </a-table>
+</template>
+  <style scoped>
+  .search {
+    margin-bottom: 20px;
+  }
+  #assignRole {
+    /* background-color: '#1677ff' */
+  }
+  /* :deep(#resetPwd  .ant-btn-default) {
+    background-color: '#1677ff';
+  } */
+  #resetPwd >:where(.css-dev-only-do-not-override-1p3hq3p).ant-btn-default {
+      background-color: orange;
+      color: white;
+  }
+</style>
  

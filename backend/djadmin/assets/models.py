322ff8b55efc1d3e41c29a40f98f9aef124b524a
chlinwei <862350707@qq.com>
2025-06-12 17:47:03 +0800

    assets modeL修改

--- backend/djadmin/assets/models.py
+++ backend/djadmin/assets/models.py
@@ -27,22 +27,28 @@ class Application(BaseModel):
 
 
 class Host(BaseModel):
-    STATUS = (
-        ('0', u'下线'),
-        ('1', u'在线'),
+    SSH_STATUS = (
+        (0, '下线'),
+        (1, '在线'),
+    )
+    OPERATION_STATUS = (
+        (0, '下线'),
+        (1, '在线'),
     )
     application = models.ForeignKey(Application,default='', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='主机类别')
-    hostname = models.CharField(default='',blank=True,null=True,max_length=200,verbose_name='主机名')
     host_credential = models.ForeignKey(Credential, default='', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='SSH用户')
+    hostname = models.CharField(default='',blank=True,null=True,max_length=200,verbose_name='主机名')
     ssh_ip = models.CharField(default='', max_length=128, null=True, blank=True, verbose_name='SSH IP地址')
     ssh_port = models.PositiveIntegerField(default=22, null=True, blank=True, verbose_name='SSH 端口')
     cpu = models.CharField(default='', max_length=64, null=True, blank=True, verbose_name='CPU')
     memory = models.CharField(default='', max_length=64, null=True, blank=True, verbose_name='内存')
     disk = models.CharField(default='', max_length=64, null=True, blank=True, verbose_name='磁盘大小')
-    system_product = models.CharField(default='', max_length=128, null=True, blank=True, verbose_name='服务器类型')
-    status = models.CharField(default='1', max_length=2, choices=STATUS, verbose_name='运行状态')
+    ssh_status = models.PositiveBigIntegerField(default=0,choices=SSH_STATUS, verbose_name='ssh状态')
+    operation_status = models.PositiveBigIntegerField(default=0,choices=OPERATION_STATUS,verbose_name='运营状态')
+    
     class Meta:
         ordering = ['-id']
         db_table = 'assets_host'
     def __str__(self):
         return self.hostname
+
